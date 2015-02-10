5bb69ad5ca588c7e209f9b37daa1111220c29a27
EmanuelaMollova <emanuela.mollova@gmail.com>
2015-02-10 22:55:02 +0200

    Add register to controller

--- controllers/website_controller.rb
+++ controllers/website_controller.rb
@@ -11,5 +11,26 @@ module Hirundo
       @title = 'Welcome to Hirundo!'
       haml :login
     end
+
+    get '/register' do
+      @title = 'Register'
+      haml :register
+    end
+
+    post '/register' do
+      username       = params[:username]
+      password       = params[:password]
+      password_again = params[:password_again]
+      email          = params[:email]
+
+      user = User.new(username, password, email)
+
+      if user.valid?
+        user.save
+        p 'Valid'
+      else
+        p "#{user.errors.messages}"
+      end
+    end
   end
 end
