e925ff6e7b78579eaaba8b9520215cbf3d2e3654
EmanuelaMollova <emanuela.mollova@gmail.com>
2015-02-11 01:39:36 +0200

    Add password confirmation in user model

--- models/user.rb
+++ models/user.rb
@@ -2,7 +2,7 @@ class User
   include Mongoid::Document
   include ActiveModel::Validations
 
-  attr_accessor :username, :email, :password, :registration_date, :is_verified
+  attr_accessor :username, :email, :password, :password_confirmation, :registration_date, :is_verified
 
   field :username, type: String
   field :email, type: String
@@ -15,12 +15,14 @@ class User
   validates_presence_of :username, message: 'Please provide an username.'
   validates_uniqueness_of :username, message: 'This username is already taken.'
   validates_length_of :password, minimum: 6, message: 'The password should be at least 6 symbols.'
+  validates_confirmation_of :password, message: 'Both passwords should match.'
 
-  def initialize(username, email, password)
+  def initialize(username, password, password_confirmation, email)
     super(
       username: username,
       email: email,
       password: password,
+      password_confirmation: password_confirmation
     )
   end
 end
