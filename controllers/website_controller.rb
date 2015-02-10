6dac8ffe7709073878c10bc2d0b5cb548de6d418
EmanuelaMollova <emanuela.mollova@gmail.com>
2015-02-10 22:55:02 +0200

    Add register to controller

--- controllers/website_controller.rb
+++ controllers/website_controller.rb
@@ -1,7 +1,5 @@
 module Hirundo
   class WebsiteController < ApplicationController
-    # helpers WebsiteHelpers
-
     not_found do
       @title = 'Not found'
       haml :not_found
@@ -11,5 +9,38 @@ module Hirundo
       @title = 'Welcome to Hirundo!'
       haml :login
     end
+
+    get '/' do
+      redirect '/login'
+    end
+
+    get '/register' do
+      @title = 'Register'
+      haml :register
+    end
+
+    post '/register' do
+      username       = params[:username]
+      password       = params[:password]
+      password_again = params[:password_again]
+      email          = params[:email]
+
+      if password == password_again
+        user = User.new(username, password, email)
+        if user.valid?
+          user.save
+          @success = 'Your registration was successful, you can login now.'
+        else
+          @error = "#{user.errors.messages}"
+        end
+      else
+        @error = 'Both passwords should match.'
+      end
+
+      redirect '/login' unless @error
+      haml :register
+    end
+
+  helpers ViewHelpers
   end
 end
