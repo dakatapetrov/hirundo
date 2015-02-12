faa9fbd4204ee9edae102e55772e6a004404c6a1
EmanuelaMollova <emanuela.mollova@gmail.com>
2015-02-12 22:00:04 +0200

    Refactor view helpers

--- helpers/view_helpers.rb
+++ helpers/view_helpers.rb
@@ -1,23 +1,27 @@
 module Hirundo
   module ViewHelpers
-    def show_error_message(error)
-      inner = case error
-              when String
-                "<p>#{error}</p>"
-              when Hash
-                error.values.inject("") { |memo, value| memo += "<p>#{value.inject("") { |m, v| m += v }}</p>" }
-              else
-              end
-
-      p error
-      p inner
-      puts error.class
-
-      "<div class='center-block alert alert-danger'>#{inner}</div>" if error
+    def show_message(message, type)
+      "<div class='center-block alert alert-#{type}'>#{message}</div>" if message
     end
 
-    def show_success_message(success)
-      "<div class='center-block alert alert-success'><span>#{success}</span></div>" if success
+    def show_error_messages
+      errors = flash[:errors]
+
+      message = case errors
+        when String
+          errors
+        when Hash
+          errors.values.inject("") do |memo, value|
+            messages = value.inject("") { |mem, msg| mem += msg }
+            memo += "<p>#{messages}</p>"
+          end
+        end
+
+      show_message(message, 'danger')
+    end
+
+    def show_success_messages
+      show_message(flash[:success], 'success')
     end
 
     def date_to_string(date)
