9c60c233a89609bf510e6a0b9c0d08c1a1f396a0
EmanuelaMollova <emanuela.mollova@gmail.com>
2015-02-13 00:58:35 +0200

    Add message model

---
+++ models/message.rb
@@ -0,0 +1,21 @@
+class Message
+  include Mongoid::Document
+  include ActiveModel::Validations
+
+  field :content, type: String
+  field :location, type: String
+  field :date, type: Time, default: ->{ Time.now }
+
+  has_one :user, class_name: "User", inverse_of: :user
+
+  validates_presence_of :content, message: 'Please provide content of the message.'
+  validates_length_of :content, maximum: 150, message: 'The message should be maximum 150 symbols long.'
+
+  def initialize(content, location, user)
+    super(
+      content: content,
+      location: location,
+      user: user
+    )
+  end
+end
