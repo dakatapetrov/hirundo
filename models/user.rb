5b890e368d1a9d43297bf86bb90269d25147aac7
EmanuelaMollova <emanuela.mollova@gmail.com>
2015-02-15 04:21:28 +0200

    Add self relation for followed users

--- models/user.rb
+++ models/user.rb
@@ -9,7 +9,7 @@ class User
   field :registration_date, type: Time, default: ->{ Time.now }
   field :is_verified, type: Boolean, default: false
 
-  has_many :follows, class_name: "User", inverse_of: :user
+  has_and_belongs_to_many :follows, class_name: "User"
 
   validates_presence_of :username, message: 'Please provide an username.'
   validates_uniqueness_of :username, message: 'This username is already taken.'
